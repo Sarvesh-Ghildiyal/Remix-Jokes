f0a771942acd07cf967aa6dc2cc842af303ab5c8
ellena <sarveshghildiyal@gmail.com>
2024-03-28 12:22:59 +0530

    done with basic setup

--- app/root.tsx
+++ app/root.tsx
@@ -1,29 +1,17 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { LiveReload, Outlet } from "@remix-run/react";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
       </head>
       <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+        Hello World
+        <Outlet />
+        <LiveReload />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
