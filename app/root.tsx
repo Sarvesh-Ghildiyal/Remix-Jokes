faf1535389bb316d99a12f5456e3c06dc9ff594d
ellena <sarveshghildiyal@gmail.com>
2024-03-28 15:53:21 +0530

    added styles

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { LiveReload, Outlet } from "@remix-run/react";
+import { Links, LiveReload, Outlet } from "@remix-run/react";
 
 export default function App() {
   return (
@@ -6,6 +6,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Links/>
       </head>
       <body>
         Hello World
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,11 @@
-export default function IndexRoute(){
-  return(
-    <p>Hello Index Route</p>
-  )
+import { LinksFunction } from "@remix-run/node";
+
+import styleUrl from "~/styles/index.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styleUrl },
+];
+
+export default function IndexRoute() {
+  return <p>Hello Index Route</p>;
 }
