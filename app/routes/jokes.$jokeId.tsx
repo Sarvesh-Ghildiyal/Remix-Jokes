92b6faf2571006687b2d466ae0a9da96b2680da2
ellena <sarveshghildiyal@gmail.com>
2024-03-30 12:57:27 +0530

    Loader Funcs, jokes-index, id, jokes

--- app/routes/jokes.$jokeId.tsx
+++ app/routes/jokes.$jokeId.tsx
@@ -1,12 +1,26 @@
 /* eslint-disable react/no-unescaped-entities */
-export default function JokeId() {
+
+import { json, type LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Link } from "react-router-dom";
+import { db } from "~/utils/db.server";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+    select: { id: true, name: true, content: true },
+  });
+  return json({ joke });
+}
+
+export default function JokesRoute() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <div>
-      <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>Here's your <strong>Hilarioud</strong> joke:</p>
+      <p>{data.joke?.content}</p>
+      <Link to=".">"{data.joke?.name}" Permalink</Link>
     </div>
   );
 }
--- app/routes/jokes._index.tsx
+++ app/routes/jokes._index.tsx
@@ -1,9 +1,31 @@
 /* eslint-disable react/no-unescaped-entities */
+
+import { json } from "@remix-run/node";
+import { useLoaderData, Link } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
+
+export async function loader() {
+  const count = await db.joke.count();
+  const randomNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    skip: randomNumber,
+    select: { id: true, name: true, content: true },
+    take: 1,
+  });
+
+  return json({ randomJoke });
+}
+
 export default function IndexJokes() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div>
-      <p>Here's a random joke:</p>
-      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+      <p>
+        Here's a <strong>Random</strong> joke:
+      </p>
+      <p>{data.randomJoke.content}</p>
+      <Link to={data.randomJoke.id}>"{data.randomJoke.name}" Permalink</Link>
     </div>
   );
 }
